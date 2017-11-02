957df3244d6a2ba27f2ce9ee3446225d431fc00e
April Denise Belcher <aprildenise@Aprils-MacBook-Pro.local>
2017-11-01 23:05:29 -0700

    added questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+//Questions array
+
+const triviaQuestions = [
+{
+Question1:" This Pokemon is a Fire-type Pokémon, and resembles a small dinosaur, its red skin and flaming tail giving it an appearance indicative of its type. Typical moves include Ember and Tail Whip.",
+Answer {"Charmander"}
+},
+{
+Question2: "They very rare, in the wild they can be found during nights with full moon on Mt. Moon and Mt. Coronet. Once they evolve they become a Clefable",
+Answer {"Clefairy"}
+},
+{
+Question3: "He is a member of Team Rocket has the unique capability of human speech."
+Ans "Meowth"
+},
+{
+Question4: "This Pokemon is a very adaptable, evolving into as many as seven types of pokemon, depending on its evolutionary situation. Some evolved creatures become flare on and vaporeon.",
+Answer: "Eevee"
+},
+{
+Question5: "He is an electric-type mouse Pokémon, and is the primary character of Pokemon",
+An: "Pikachu"
+}];
+
+console.log (triviaQuestions);
